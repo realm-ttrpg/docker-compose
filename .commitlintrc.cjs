3dd0572effe768a08695d8096699006a5f2b6b64
haliphax <haliphax@gmail.com>
2025-03-11 22:53:19 -0500

    🚨 prettier

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-  extends: ["gitmoji"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^[^ ]+ (.*)$/,
-      headerCorrespondence: ["subject"],
-    },
-  },
-  rules: {
-    "type-empty": [0, "always"],
-  },
+	extends: ["gitmoji"],
+	parserPreset: {
+		parserOpts: {
+			headerPattern: /^[^ ]+ (.*)$/,
+			headerCorrespondence: ["subject"],
+		},
+	},
+	rules: {
+		"type-empty": [0, "always"],
+	},
 };
--- .husky/scripts/no-commit-to-main.js
+++ .husky/scripts/no-commit-to-main.js
@@ -3,11 +3,11 @@ import child_process from "node:child_process";
 if (process.env.ALLOW_MAIN) process.exit(0);
 
 const branch = child_process
-  .execSync("git branch --show-current")
-  .toString()
-  .trim();
+	.execSync("git branch --show-current")
+	.toString()
+	.trim();
 
 if (branch === "main") {
-  process.stderr.write("❌ Do not commit to main branch\n");
-  process.exit(1);
+	process.stderr.write("❌ Do not commit to main branch\n");
+	process.exit(1);
 }
